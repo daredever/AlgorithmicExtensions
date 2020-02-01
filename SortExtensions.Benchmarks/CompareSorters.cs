b797ea2add6132e905b68883146fc35f36d1b316
Roman <daredever@gmail.com>
2020-02-01 12:14:23 +0300

    add memory diagnoser

--- SortExtensions.Benchmarks/CompareSorters.cs
+++ SortExtensions.Benchmarks/CompareSorters.cs
@@ -6,11 +6,12 @@ using SortExtensions.Sorters.Implementations;
 
 namespace SortExtensions.Benchmarks
 {
+    [MemoryDiagnoser]
     public class CompareSorters
     {
         private readonly int[] _data;
-        private const int StartIndex = 0;
-        private const int Length = 1000;
+        private const int Index = 0;
+        private const int Length = 10000;
 
         private readonly IComparer<int> _comparer = Comparer<int>.Default;
 
@@ -23,25 +24,25 @@ namespace SortExtensions.Benchmarks
         public CompareSorters()
         {
             _data = new int[Length];
-            for (var i = 0; i < _data.Length; i++)
+            for (var i = Index; i < _data.Length; i++)
             {
                 _data[i] = new Random(42).Next(-1000, 1000);
             }
         }
 
         [Benchmark]
-        public IList<int> BubbleSort() => _bubbleSorter.Sort(_data, StartIndex, Length, _comparer);
+        public IList<int> BubbleSort() => _bubbleSorter.Sort(_data, Index, Length, _comparer);
 
         [Benchmark]
-        public IList<int> InsertionSort() => _insertionSorter.Sort(_data, StartIndex, Length, _comparer);
+        public IList<int> InsertionSort() => _insertionSorter.Sort(_data, Index, Length, _comparer);
 
         [Benchmark]
-        public IList<int> MergeSort() => _mergeSorter.Sort(_data, StartIndex, Length, _comparer);
+        public IList<int> MergeSort() => _mergeSorter.Sort(_data, Index, Length, _comparer);
 
         [Benchmark]
-        public IList<int> DefaultSort() => _defaultSorter.Sort(_data, StartIndex, Length, _comparer);
+        public IList<int> DefaultSort() => _defaultSorter.Sort(_data, Index, Length, _comparer);
 
         [Benchmark]
-        public IList<int> SelectionSort() => _selectionSorter.Sort(_data, StartIndex, Length, _comparer);
+        public IList<int> SelectionSort() => _selectionSorter.Sort(_data, Index, Length, _comparer);
     }
 }
