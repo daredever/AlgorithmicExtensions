51623844ec0fa8335bfb27888ce16ae2ede30488
Roman <daredever@gmail.com>
2020-05-01 19:27:14 +0300

    fix comments

--- src/SortExtensions/ListExtensions.cs
+++ src/SortExtensions/ListExtensions.cs
@@ -24,8 +24,7 @@ namespace SortExtensions
         /// <exception cref="ArgumentOutOfRangeException">collection bounds less than range</exception>
         /// <exception cref="ArgumentOutOfRangeException">wrong sorting algorithm specified</exception>
         public static IList<T> Sort<T>(this IList<T> source, SortingAlgorithm sortingAlgorithm,
-            IComparer<T> comparer = null)
-            where T : IComparable
+            IComparer<T> comparer = null) where T : IComparable
         {
             return SortInternal(source, 0, source.Count, GetSorter(sortingAlgorithm), comparer);
         }
@@ -65,8 +64,7 @@ namespace SortExtensions
         /// <exception cref="ArgumentOutOfRangeException">collection bounds less than range</exception>
         /// <exception cref="ArgumentOutOfRangeException">wrong sorting algorithm specified</exception>
         public static IList<T> Sort<T>(this IList<T> source, int index, int length, SortingAlgorithm sortingAlgorithm,
-            IComparer<T> comparer = null)
-            where T : IComparable
+            IComparer<T> comparer = null) where T : IComparable
         {
             return SortInternal(source, index, length, GetSorter(sortingAlgorithm), comparer);
         }
@@ -89,8 +87,7 @@ namespace SortExtensions
         /// <exception cref="ArgumentOutOfRangeException">collection bounds less than range</exception>
         /// <exception cref="ArgumentOutOfRangeException">wrong sorting algorithm specified</exception>
         public static IList<T> Sort<T>(this IList<T> source, Range range, SortingAlgorithm sortingAlgorithm,
-            IComparer<T> comparer = null)
-            where T : IComparable
+            IComparer<T> comparer = null) where T : IComparable
         {
             return SortInternal(source, range, GetSorter(sortingAlgorithm), comparer);
         }
@@ -109,18 +106,13 @@ namespace SortExtensions
         /// <exception cref="ArgumentOutOfRangeException">index or length less than zero</exception>
         /// <exception cref="ArgumentOutOfRangeException">collection bounds less than range</exception>
         public static IList<T> Sort<T>(this IList<T> source, int index, int length, ISorter sorter,
-            IComparer<T> comparer = null)
-            where T : IComparable
+            IComparer<T> comparer = null) where T : IComparable
         {
             return SortInternal(source, index, length, sorter, comparer);
         }
 
         /// <summary>
         /// Sorts the elements of a zero-based collection with specified sorter.
-        /// The sort compares the elements to each other using the IComparable interface,
-        /// which must be implemented by all elements in the given section of the collection.
-        /// If comparer is null, the elements are compared to each other using the IComparable
-        /// interface, which in that case must be implemented by all elements of the collection.
         /// </summary>
         /// <param name="source">Collection of elements</param>
         /// <param name="range">Range for sorting</param>
